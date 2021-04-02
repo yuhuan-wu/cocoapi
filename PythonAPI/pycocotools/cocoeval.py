143563fe819d47080aabe1b5d6d4bb85669b8844
Yu-Huan Wu <wuyuhuan@mail.nankai.edu.cn>
2021-04-02 10:15:40 +0800

    Detection Evaluation Onput AP70 Instead

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -459,7 +459,7 @@ class COCOeval:
             stats = np.zeros((12,))
             stats[0] = _summarize(1)
             stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
+            stats[2] = _summarize(1, iouThr=.7, maxDets=self.params.maxDets[2])
             stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
             stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
             stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
